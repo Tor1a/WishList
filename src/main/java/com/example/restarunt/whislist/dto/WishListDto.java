638ed780bb79c6fe233f5d4a343c7ec4c2df36d3
Tor1a <se_o_ul@naver.com>
2023-03-20 23:38:20 +0900

    entity, service 구현

--- src/main/java/com/example/restarunt/whislist/dto/WishListDto.java
+++ src/main/java/com/example/restarunt/whislist/dto/WishListDto.java
@@ -1,6 +1,5 @@
-package com.example.restarunt.whislist.entity;
+package com.example.restarunt.whislist.dto;
 
-import com.example.restarunt.db.MemoryDbEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +9,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class WishListEntity extends MemoryDbEntity {
+public class WishListDto {
+    private Integer index;
     private String title; // 음식명,장소명
     private String category; // 카테고리
     private String address; // 주소
@@ -20,5 +20,4 @@ public class WishListEntity extends MemoryDbEntity {
     private boolean isVisit; // 방문여부
     private int visitCount; // 방문카운트
     private LocalDateTime lastVisitDate; // 마지막 방문 일자
-
 }
---
+++ src/main/java/com/example/restarunt/whislist/service/WishListService.java
@@ -0,0 +1,90 @@
+package com.example.restarunt.whislist.service;
+
+import com.example.restarunt.naver.NaverClient;
+import com.example.restarunt.naver.dto.SearchImageReq;
+import com.example.restarunt.naver.dto.SearchLocalReq;
+import com.example.restarunt.naver.dto.SearchLocalRes;
+import com.example.restarunt.whislist.dto.WishListDto;
+import com.example.restarunt.whislist.entity.WishListEntity;
+import com.example.restarunt.whislist.repository.WishListRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WishListService {
+    private final NaverClient naverClient;
+
+    private final WishListRepository wishListRepository;
+    public WishListDto search(String query){
+        //지역검색
+        var searchLocalReq = new SearchLocalReq();
+        searchLocalReq.setQuery(query);
+
+        var searchLocalRes = naverClient.searchLocal(searchLocalReq);
+
+        if(searchLocalRes.getTotal() > 0){
+            var localItem = searchLocalRes.getItems().stream().findFirst().get();
+
+            var imageQuery = localItem.getTitle().replaceAll("<[^>]*>","");
+            var searchImageReq = new SearchImageReq();
+            searchImageReq.setQuery(imageQuery);
+            // 이미지 검색
+            var searchImageRes = naverClient.searchImage(searchImageReq);
+
+            // 결과를 리턴
+            if(searchLocalRes.getTotal() > 0){
+                var imageItem = searchImageRes.getItems().stream().findFirst().get();
+
+                var result = new WishListDto();
+                result.setTitle(localItem.getTitle());
+                result.setCategory(localItem.getCategory());
+                result.setAddress(localItem.getAddress());
+                result.setRoadAddress(localItem.getRoadAddress());
+                result.setHomePageLink(localItem.getLink());
+                result.setImageLink(imageItem.getLink());
+
+                return result;
+            }
+        }
+        return new WishListDto();
+
+
+    }
+
+    public WishListDto add(WishListDto wishListDto) {
+        var entity = dtoToEntity(wishListDto);
+        var saveEntity = wishListRepository.save(entity);
+        return entityToDto(saveEntity);
+    }
+
+    private WishListEntity dtoToEntity(WishListDto wishListDto){
+        var entity = new WishListEntity();
+        entity.setIndex(wishListDto.getIndex());
+        entity.setTitle(wishListDto.getTitle());
+        entity.setCategory(wishListDto.getCategory());
+        entity.setAddress(wishListDto.getAddress());
+        entity.setRoadAddress(wishListDto.getRoadAddress());
+        entity.setHomePageLink(wishListDto.getHomePageLink());
+        entity.setImageLink(wishListDto.getImageLink());
+        entity.setVisit(wishListDto.isVisit());
+        entity.setVisitCount(wishListDto.getVisitCount());
+        entity.setLastVisitDate(wishListDto.getLastVisitDate());
+        return entity;
+    }
+
+    private WishListDto entityToDto(WishListEntity wishListEntity){
+        var dto = new WishListDto();
+        dto.setIndex(wishListEntity.getIndex());
+        dto.setTitle(wishListEntity.getTitle());
+        dto.setCategory(wishListEntity.getCategory());
+        dto.setAddress(wishListEntity.getAddress());
+        dto.setRoadAddress(wishListEntity.getRoadAddress());
+        dto.setHomePageLink(wishListEntity.getHomePageLink());
+        dto.setImageLink(wishListEntity.getImageLink());
+        dto.setVisit(wishListEntity.isVisit());
+        dto.setVisitCount(wishListEntity.getVisitCount());
+        dto.setLastVisitDate(wishListEntity.getLastVisitDate());
+        return dto;
+    }
+}
